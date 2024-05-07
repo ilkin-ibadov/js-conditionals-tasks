3ac1151b24c6aaa8e0ac202f5470e1541a030136
ilkin-ibadov <ilkin.ibadov21@gmail.com>
2024-05-07 23:33:27 +0400

    all

---
+++ index.js
@@ -0,0 +1,37 @@
+/*
+Grade Classifier: Write a function that takes a student's score as input and
+returns a grade (A, B, C, D, or F) based on the following grading scale:
+A: 90-100
+B: 80-89
+C: 70-79
+D: 60-69
+F: 0-59
+*/
+
+/*
+Time Converter: Write a function that takes a time in 24-hour format (e.g., "13:45")
+as input and returns the time in 12-hour format with AM/PM (e.g., "1:45 PM").
+*/
+
+/*
+Password Strength Checker: Write a function that takes a password as input and returns
+a message indicating its strength:
+"Weak" if the password has fewer than 8 characters,
+"Medium" if it has 8 to 12 characters,
+"Strong" if it has more than 12 characters and contains at least one digit, one lowercase
+letter, and one uppercase letter.
+*/
+
+/*
+Write a function that takes weekday's number (from 1 to 7) as an input and returns the
+appropriate weekday name
+*/
+
+/*
+Taxi Fare Calculator: Write a function that takes the distance traveled (in kilometers)
+and the waiting time (in minutes) as input and calculates the taxi fare according to the
+following rules:
+Base fare: $5
+Additional fee: $0.50 per kilometer
+Additional fee for every 3 minutes waiting time: $1
+*/
